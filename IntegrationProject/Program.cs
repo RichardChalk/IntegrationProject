1c7eda7a06990362f03a1b993d2ee6ab864cbe29
Richard Chalk <barmybrit@gmail.com>
2023-05-05 06:08:00 +0200

    Print 2 people to console

--- IntegrationProject/Program.cs
+++ IntegrationProject/Program.cs
@@ -1,2 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using IntegrationProject.Models;
+using IntegrationProject.Services;
+
+var people = new List<FakePerson>();
+var service = new FakePersonService();
+
+for (int i = 0; i < 2; i++)
+{
+    people.Add(service.Generate());
+}
+
+
+foreach (var p in people)
+{
+    Console.WriteLine($"{p.FirstName} {p.LastName}");
+}
+
+Console.ReadLine();
